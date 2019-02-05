82d8ecb01deb034a9dbedd5b7f3da54eb78efac0
JETZ <jeremyetzine@hotmail.com>
2019-02-06 10:40:26 +1100

    added join table and edit forms

---
+++ db/migrate/20190205233342_flights_users.rb
@@ -0,0 +1,8 @@
+class FlightsUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :flights_users, :id => false do |t|
+      t.integer :flight_id
+      t.integer :user_id
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_05_055535) do
+ActiveRecord::Schema.define(version: 2019_02_05_233342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 2019_02_05_055535) do
     t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "flights_users", id: false, force: :cascade do |t|
+    t.integer "flight_id"
+    t.integer "user_id"
   end
 
   create_table "reservations", force: :cascade do |t|
