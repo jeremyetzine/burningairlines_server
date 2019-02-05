5dd7a5c58a5f6ca23efc9fa32654673ece7d4cc3
JETZ <jeremyetzine@hotmail.com>
2019-02-06 00:52:29 +1100

    flights basic crud

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -69,6 +69,6 @@ class FlightsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def flight_params
-      params.fetch(:flight, {})
+      params.require(:flight).permit(:flight, :date, :to, :from, :time, :airplane_id)
     end
 end
